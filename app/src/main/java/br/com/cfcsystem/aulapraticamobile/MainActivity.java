93e3c067b48845db3719f877d2bf7ed0bfd136c9
cfcAnderson <asfcarvalho13@gmail.com>
2015-02-19 14:24:43 -0300

    Anderson

--- app/src/main/java/br/com/cfcsystem/aulapraticamobile/MainActivity.java
+++ app/src/main/java/br/com/cfcsystem/aulapraticamobile/MainActivity.java
@@ -1,28 +1,71 @@
 package br.com.cfcsystem.aulapraticamobile;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import br.com.cfcsystem.classes.Biometria;
 
 
 public class MainActivity extends Activity {
 
+    private Biometria biometria = new Biometria();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button bttInit = (Button) findViewById(R.id.initAula);
+
+        bttInit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //chamar a thread!
+            }
+        });
 
     }
 
     public void onBiometriaClick(View v){
 
-        startActivity(new Intent(getBaseContext(),Biometria.class));
+        //startActivity(new Intent(getBaseContext(),Biometria.class));
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                MainActivity.this);
+
+        final View viewBiometria = View.inflate(this, R.layout.activity_biometria, null);
+
+
+        alertDialogBuilder.setView(viewBiometria);
+        alertDialogBuilder.setTitle("Legendas");
+
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("OK",new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        biometria.getBiometria();
+                    }
+                })
+                .setNegativeButton("Instrutor", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // se não for precionado ele apenas termina o dialog
+                        // e fecha a janelinha
+
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        alertDialog.show();
+
     }
 
 
--- app/src/main/java/br/com/cfcsystem/classes/Biometria.java
+++ app/src/main/java/br/com/cfcsystem/classes/Biometria.java
@@ -1,32 +1,21 @@
 package br.com.cfcsystem.classes;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
 import com.nitgen.SDK.AndroidBSP.NBioBSPJNI;
 
-import br.com.cfcsystem.aulapraticamobile.R;
-
-public class Biometria extends Activity {
+public class Biometria{
 
     private NBioBSPJNI				bsp;
     private NBioBSPJNI.Export       exportEngine;
     private NBioBSPJNI.IndexSearch  indexSearch;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_biometria);
-
+    public Biometria(){
         InitData();
     }
 
     private void InitData(){
         NBioBSPJNI.CURRENT_PRODUCT_ID = 0;
         if(bsp==null){
-            bsp = new NBioBSPJNI("010701-613E5C7F4CC7C4B0-72E340B47E034015", this);
+            bsp = new NBioBSPJNI("010701-613E5C7F4CC7C4B0-72E340B47E034015");
             String msg = null;
             if (bsp.IsErrorOccured())
                 msg = "NBioBSP Error: " + bsp.GetErrorCode();
@@ -42,26 +31,9 @@ public class Biometria extends Activity {
         //userDialog = new UserDialog();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_biometria, menu);
-        return true;
+    public String getBiometria(){
+        return null;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
