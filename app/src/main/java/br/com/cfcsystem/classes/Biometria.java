6c7609c25b4eb67501ba45a1519c916df6b5899a
cfcAnderson <asfcarvalho13@gmail.com>
2015-03-09 13:18:06 -0400

    Revert "Anderson"
    This reverts commit 264bdb007f2860ca362f0ed9a23105ad51a49c0f.
    
    Conflicts:
    	Aula_Pratica_Desktop/src/classes/Parametros.java

--- app/src/main/java/br/com/cfcsystem/classes/Biometria.java
+++ app/src/main/java/br/com/cfcsystem/classes/Biometria.java
@@ -1,14 +1,21 @@
 package br.com.cfcsystem.classes;
 
+import android.app.DialogFragment;
+import android.content.Context;
+
 import com.nitgen.SDK.AndroidBSP.NBioBSPJNI;
 
-public class Biometria{
+public class Biometria implements NBioBSPJNI.CAPTURE_CALLBACK{
 
     private NBioBSPJNI				bsp;
     private NBioBSPJNI.Export       exportEngine;
     private NBioBSPJNI.IndexSearch  indexSearch;
+    private DialogFragment sampleDialogFragment;
+    private Context context = null;
 
-    public Biometria(){
+
+    public Biometria(Context context){
+        this.context = context;
         InitData();
     }
 
@@ -24,16 +31,55 @@ public class Biometria{
                 exportEngine = bsp.new Export();
                 indexSearch = bsp.new IndexSearch();
             }
-            //tvVer.setText(msg);
         }
-
-        //sampleDialogFragment = new SampleDialogFragment();
-        //userDialog = new UserDialog();
     }
 
     public String getBiometria(){
+
+        new Thread(new Runnable() {
+
+            public void run() {
+
+                OnCapture1(10000);
+
+            }
+        }).start();
         return null;
     }
 
+    String msg = "";
+    public synchronized void OnCapture1(int timeout){
 
+        NBioBSPJNI.FIR_HANDLE hCapturedFIR, hAuditFIR;
+        NBioBSPJNI.CAPTURED_DATA capturedData;
+
+        hCapturedFIR = bsp.new FIR_HANDLE();
+        hAuditFIR = bsp.new FIR_HANDLE();
+        capturedData = bsp.new CAPTURED_DATA();
+
+        bsp.Capture(NBioBSPJNI.FIR_PURPOSE.ENROLL,hCapturedFIR,timeout, hAuditFIR, capturedData, null,0, null);
+
+        if (bsp.IsErrorOccured())  {
+            msg = "NBioBSP Capture Error: " + bsp.GetErrorCode();
+        }else{
+            System.out.println("biometria: "+ hCapturedFIR.toString());
+        }
+
+    }
+
+
+    @Override
+    public int OnCaptured(NBioBSPJNI.CAPTURED_DATA captured_data) {
+        return 0;
+    }
+
+    @Override
+    public void OnConnected() {
+
+    }
+
+    @Override
+    public void OnDisConnected() {
+
+    }
 }
